45842c6717aa57219607edb8001e8e3f536f657e
Đại Lê <33823464+Takasi3@users.noreply.github.com>
2018-10-11 01:16:03 +0700

    Hoàn Thành
    Giáo Sư đã thành công

--- footwear/admin/addProduct.php
+++ footwear/admin/addProduct.php
@@ -1,48 +1,70 @@
 <?php
-if(isset($_POST['name']))
-{
+if (isset($_POST['name'])) {
+} else {
+    header("location:form.php");
 }
-else
-{
-	header("location:form.php");
-}
-$name = $_POST['name'];
-$type_id = $_POST['type_id'];
-$manu_id = $_POST['manu_id'];
+$name        = $_POST['name'];
+$type_id     = $_POST['type_id'];
+$manu_id     = $_POST['manu_id'];
 $Dessciption = $_POST['Des'];
-$Price = $_POST['Pri'];
+$Price       = $_POST['Pri'];
 
-
-
-
-$target_dir = "public/images/products/";
-$target_file = $target_dir . basename($_FILES["fileUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+$target_dir    = "public/images/products/";
+$target_file   = $target_dir . basename($_FILES["fileUpload"]["name"]);
+$uploadOk      = 1;
+$imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 // Check if image file is a actual image or fake image
-if(isset($_POST["name"])) {
+if (isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileUpload"]["tmp_name"]);
-   			if($check !== false) {
-        		echo "File is an image - " . $check["mime"] . ".";
-        		$uploadOk = 1;
-        	} else {
-        		echo "File is not an image.";
-       			 $uploadOk = 0;
-   				}
-    	if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-		&& $imageFileType != "gif" ) {
-    	echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-    	$uploadOk = 0;
-							}
+    if ($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+// Check file size
+if ($_FILES["fileUpload"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+    && $imageFileType != "gif") {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fileUpload"]["tmp_name"], $target_file)) {
+        echo "The file " . basename($_FILES["fileUpload"]["name"]) . " has been uploaded.";
+        require "config.php";
+        require "db.php";
+        $db    = new db;
+        $image = $_FILES["fileUpload"]["name"];
+        $db->AddProducts($name, $Price, $image, $Dessciption, $manu_id, $type_id);
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
 
-							}
-require "config.php";
-	require "db.php";
-	$db = new db;
-	$image = $_FILES["fileUpload"]["name"];
-	echo $image;
-	$db -> AddProducts($name , $Price ,$image,$Dessciption,$manu_id,$type_id);
-	//header("location:index.php");
-	
+if ($uploadOk != 0) {
+    ?>
+	<html>
+<head>
+<META http-equiv="refresh" content="5;URL=index.php">
+</head>
+</html>
 
-?>
+<?php }?>
+
+// header("location:index.php");
--- footwear/admin/db.php
+++ footwear/admin/db.php
@@ -1,157 +1,151 @@
 <?php
-class db{
-	// tao bien ket noi
-	public static $conn;
+class db
+{
+    // tao bien ket noi
+    public static $conn;
 
-	// viet ham ket noi
-	public function __construct(){
-		self::$conn =  new mysqli(DB_HOST , DB_USER , DB_PASS,DB_NAME);
-		// hien thi ieng viet
-	self::$conn -> set_charset('utf8');
-	}
-	
-	// dong ket noi
+    // viet ham ket noi
+    public function __construct()
+    {
+        self::$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+        // hien thi ieng viet
+        self::$conn->set_charset('utf8');
+    }
 
-	public function __destruct(){
-		self::$conn->close();
-	}
-	// ham tra ve array tu object
-	public function getData($obj){
-		$arr = array();
-		while ($row = $obj->fetch_assoc()){
-			$arr[] = $row;
-		}
-		return $arr;
-	}
+    // dong ket noi
 
+    public function __destruct()
+    {
+        self::$conn->close();
+    }
+    // ham tra ve array tu object
+    public function getData($obj)
+    {
+        $arr = array();
+        while ($row = $obj->fetch_assoc()) {
+            $arr[] = $row;
+        }
+        return $arr;
+    }
 
+    // Ham lay ra tat ca san pham khong phan trang
+    public function getAllProducts2()
+    {
 
-	// Ham lay ra tat ca san pham khong phan trang
-	public function getAllProducts2(){
+        // stt trang bat dau
 
-	 	// stt trang bat dau
-	 	
-	 	$sql ="SELECT * FROM `protypes` ,`products`,`manufactures` WHERE protypes.type_ID = products.type_ID AND manufactures.manu_ID = products.manu_ID ORDER BY ID DESC";
-	 		return  self::$conn -> query($sql);
-	 }
-	
+        $sql = "SELECT * FROM `protypes` ,`products`,`manufactures` WHERE protypes.type_ID = products.type_ID AND manufactures.manu_ID = products.manu_ID ORDER BY ID DESC";
+        return self::$conn->query($sql);
+    }
 
+    // Ham lay ra tat ca san pham co phan trang
+    public function getAllProducts($page, $per_page)
+    {
 
+        // stt trang bat dau
+        $first_link = ($page - 1) * $per_page;
+        $sql        = "SELECT * FROM `protypes` ,`products`,`manufactures` WHERE protypes.type_ID = products.type_ID AND manufactures.manu_ID = products.manu_ID ORDER BY ID DESC LIMIT $first_link,$per_page";
+        // thuc thi cau truy van
+        $result = self::$conn->query($sql);
+        return $this->getData($result);
+    }
 
+    // Ham Phan Trang 1,2,3..next , last
+    public function paginate($url, $total, $page, $per_page)
+    {
+        if ($total <= 0) {
+            return "";
+        }
+        $total_links = ceil($total / $per_page);
+        if ($total_links <= 1) {
+            return "";
+        }
+        $link       = "";
+        $first_link = "";
+        $last_link  = "";
 
+        if ($page > 1) {
 
-	 	// Ham lay ra tat ca san pham co phan trang
-	 public function getAllProducts($page,$per_page){
+            $first_link = "<li><a href='$url'> First    </a></li>";
+        }
+        for ($i = 1; $i <= $total_links; $i++) {
+            $link = $link . "<li><a href='$url?page=$i'>$i  </a></li>";
+        }
+        if ($page < $total_links) {
 
-	 	// stt trang bat dau
-	 	$first_link=($page-1)*$per_page;
-	 	$sql ="SELECT * FROM `protypes` ,`products`,`manufactures` WHERE protypes.type_ID = products.type_ID AND manufactures.manu_ID = products.manu_ID ORDER BY ID DESC LIMIT $first_link,$per_page";
-	 	// thuc thi cau truy van
-	 	$result = self::$conn -> query($sql);
-	 	return $this ->getData($result);
-	 }
+            $last_link = "<li><a href='$url?page=$total_links'> Last</a></li>";
+        }
+        return $first_link . $link . $last_link;
+    }
 
-	 // Ham Phan Trang 1,2,3..next , last
-	  public function paginate($url,$total,$page,$per_page)
-	  {
-	  	if($total <= 0) {
-				     return "";
-				     }
-				     $total_links = ceil($total/$per_page);
-				     if($total_links <= 1) {
-				     return "";
-				     }
-	  	$link="";$first_link = "";  $last_link="";  
+    // Ham lay hang~ san pham
+    public function getManuProducts()
+    {
+        $sql = "SELECT * FROM `manufactures`";
+        // thuc thi cau truy van
+        $result = self::$conn->query($sql);
+        return $this->getData($result);
+    }
+    // Ham lay Loai san pham
+    public function getTypeProducts()
+    {
+        $sql = "SELECT * FROM `protypes`";
+        // thuc thi cau truy van
+        $result = self::$conn->query($sql);
+        return $this->getData($result);
+    }
+    // Ham Xoa san pham = id
+    public function DeleteByID($id)
+    {
+        $sql = "DELETE FROM `products` WHERE ID = $id";
+        // thuc thi cau truy van
+        $result = self::$conn->query($sql);
+        return $result;
+    }
+    // Ham Xoa hang~ = id
+    public function DeleteManuByID($id)
+    {
+        $sql = "DELETE FROM `manufactures` WHERE manu_ID = $id";
+        // thuc thi cau truy van
+        $result = self::$conn->query($sql);
+        return $result;
+    }
+    // Ham Xoa Nhan~ Hieu = id
+    public function DeleteTypeByID($id)
+    {
+        $sql = "DELETE FROM `protypes` WHERE type_ID = $id";
+        // thuc thi cau truy van
+        $result = self::$conn->query($sql);
+        return $result;
+    }
+    // Ham tim san pham = id co phan trang
+    public function SearchByID($id, $page, $per_page)
+    {
+        $first_link = ($page - 1) * $per_page;
+        $sql        = "SELECT * FROM `protypes` ,`products`,`manufactures` WHERE (protypes.type_ID = products.type_ID AND manufactures.manu_ID = products.manu_ID) AND (description LIKE N'%$id %' OR name LIKE N'%$id%') ORDER BY ID DESC LIMIT $first_link,$per_page";
+        // thuc thi cau truy van
+        $result = self::$conn->query($sql);
+        return $this->getData($result);
+    }
+    // Ham tim san pham = id khong phan trang
+    public function SearchByID2($id)
+    {
+        $sql = "SELECT * FROM `protypes` ,`products`,`manufactures` WHERE (protypes.type_ID = products.type_ID AND manufactures.manu_ID = products.manu_ID) AND (description LIKE N'%$id %' OR name LIKE N'%$id%') ORDER BY ID DESC";
+        // thuc thi cau truy van
+        return self::$conn->query($sql);
+    }
+    public function AddProducts($name, $price, $image, $description, $manu_ID, $type_ID)
+    {
+        $sql = "INSERT INTO `products`(`name`, `price`, `image`, `description`, `manu_ID`, `type_ID`) VALUES ('$name',$price,'$image','$description',$manu_ID,$type_ID)";
+        var_dump($sql);
+        return self::$conn->query($sql);
+    }
+    public function getNameProtypes()
+    {
+        $sql    = "SELECT *FROM `protypes`";
+        $result = self::$conn->query($sql);
+        return $this->getData($result);
 
-				if ($page > 1) { 
-				    
-				$first_link = "<li><a href='$url'> First    </a></li>"; 
-				} 
-	  	for($i =1 ; $i <= $total_links;$i++)
-	  	{
-	  		$link = $link."<li><a href='$url?page=$i'>$i  </a></li>";
-	  	}
-	  	if($page < $total_links) { 
-				   
-				 
-				$last_link = "<li><a href='$url?page=$total_links'> Last</a></li>";     
-				}
-	  	return $first_link.$link.$last_link;
-	  }
-
-	  
-
-	  	// Ham lay hang~ san pham
-	 public function getManuProducts(){
-	 	$sql ="SELECT * FROM `manufactures`";
-	 	// thuc thi cau truy van
-	 	$result = self::$conn -> query($sql);
-	 	return $this ->getData($result);
-	 }
-  	// Ham lay Loai san pham
-	 public function getTypeProducts(){
-	 	$sql ="SELECT * FROM `protypes`";
-	 	// thuc thi cau truy van
-	 	$result = self::$conn -> query($sql);
-	 	return $this ->getData($result);
-	 }
-	   	// Ham Xoa san pham = id
-	 public function DeleteByID($id){
-	 	$sql ="DELETE FROM `products` WHERE ID = $id";
-	 	// thuc thi cau truy van
-	 	$result = self::$conn -> query($sql);
-	 	return $result;
-	 }
-	    	// Ham Xoa hang~ = id
-	 public function DeleteManuByID($id){
-	 	$sql ="DELETE FROM `manufactures` WHERE manu_ID = $id";
-	 	// thuc thi cau truy van
-	 	$result = self::$conn -> query($sql);
-	 	return $result;
-	 }
-	 	    	// Ham Xoa Nhan~ Hieu = id
-	 public function DeleteTypeByID($id){
-	 	$sql ="DELETE FROM `protypes` WHERE type_ID = $id";
-	 	// thuc thi cau truy van
-	 	$result = self::$conn -> query($sql);
-	 	return $result;
-	 }
-	 	 	    	// Ham tim san pham = id co phan trang
-	 public function SearchByID($id,$page,$per_page){
-	 	$first_link=($page-1)*$per_page;
-	 	$sql ="SELECT * FROM `protypes` ,`products`,`manufactures` WHERE (protypes.type_ID = products.type_ID AND manufactures.manu_ID = products.manu_ID) AND (description LIKE N'%$id %' OR name LIKE N'%$id%') ORDER BY ID DESC LIMIT $first_link,$per_page";
-	 	// thuc thi cau truy van
-	 	$result = self::$conn -> query($sql);
-	 	return $this ->getData($result);
-	 }
-	     	// Ham tim san pham = id khong phan trang
-	 public function SearchByID2($id){
-	 	$sql ="SELECT * FROM `protypes` ,`products`,`manufactures` WHERE (protypes.type_ID = products.type_ID AND manufactures.manu_ID = products.manu_ID) AND (description LIKE N'%$id %' OR name LIKE N'%$id%') ORDER BY ID DESC";
-	 	// thuc thi cau truy van
-	 	return self::$conn -> query($sql);
-	 }
-	 public function AddProducts($name , $price ,$image,$description,$manu_ID,$type_ID){
-	 	$sql="INSERT INTO `products`(`name`, `price`, `image`, `description`, `manu_ID`, `type_ID`) VALUES ($name,$price,$image,$description,$manu_ID,$type_ID)";
-	 	var_dump($sql);
-	 	return self::$conn -> query($sql);
-	 }
-	 public function getNameProtypes()
-	 {
-	 	$sql ="SELECT *FROM `protypes`";
-	 	$result = self::$conn -> query($sql);
-	 	return $this ->getData($result);
-
-	 }
-
-
-
-
-	
-
-
-
-
-
-
+    }
 
 }
